5b789f012b8cbdbc3b474db9ce6f3993350a7816
jason brown <j.grant.brown@gmail.com>
2016-12-20 23:52:24 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(time)
+  x=0
+  while x<time
+    puts "#{time} SECOND(S)!"
+    time-=1
+
+    end
+    return "HAPPY NEW YEAR!"
+
 end
+
+def countdown_with_sleep(time)
+  x=0
+  while x<time
+    puts "#{time} SECOND(S)!"
+    time-=1
+    sleep 5
+    end
+    return "HAPPY NEW YEAR!"
+
+  end
